189a3081cd6bd0705868d53c997a8fd593ab3c33
taraxdev <techworks29@gmail.com>
2021-02-24 18:32:30 +0530

    admin control of slider

--- Hireme/webpages/admin.py
+++ Hireme/webpages/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
+from .models import Slider
+admin.site.register(Slider)
 # Register your models here.
--- Hireme/webpages/models.py
+++ Hireme/webpages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Slider(models.Model):
+    headline = models.CharField(max_length = 225)
+    subtitle = models.CharField(max_length = 225)
+    button_text = models.CharField(max_length = 225)
+    photo = models.ImageField(upload_to = 'media/slider/%Y')
+    created_date = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.headline
