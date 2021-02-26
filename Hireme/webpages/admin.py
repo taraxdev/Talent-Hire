e932f04fa3434de738b8c8feee5ca049538c16dd
taraxdev <techworks29@gmail.com>
2021-02-26 18:34:27 +0530

    connected team carousal to django admin

--- Hireme/webpages/admin.py
+++ Hireme/webpages/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Slider
+from .models import Slider, Team
 admin.site.register(Slider)
+admin.site.register(Team)
 # Register your models here.
